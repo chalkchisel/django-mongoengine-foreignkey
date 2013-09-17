5e06d1dbeabb1928f41ded5963eb4c6be2a9387e
Josh Ourisman <josh@joshourisman.com>
2013-09-17 11:50:26 -0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import mongofk
+
+from distutils.core import setup
+
+
+description = "An implementation of a foreign key field for mongoengine " \
+    "that references a Django model object."
+
+setup(
+    name='django-tablib',
+    version=mongofk.__version__,
+    author='Joshua Ourisman',
+    author_email='josh@bolsterlabs.com',
+    url='https://github.com/bolster/django-mongoengine-foreignkey',
+    description=description,
+    long_description=description,
+    license='BSD',
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    packages=['mongofk', ],
+)
